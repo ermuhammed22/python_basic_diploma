c2afbabaeba9308c5d7d5720b24da513e52f3500
рмухаммед асымбеков <ermuhammed_erema@mail.ru>
2024-04-01 11:25:16 +0500

    add hello command

--- config_data/config.py
+++ config_data/config.py
@@ -6,9 +6,10 @@ if not find_dotenv():
 else:
     load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-RAPID_API_KEY = os.getenv("RAPID_API_KEY")
+BOT_TOKEN = os.getenv("7110090587:AAHlTBPP8-4mKgKp51c-1_H11yb11Jur380")
+RAPID_API_KEY = os.getenv("dict.1.1.20240321T090041Z.519173ee60c3f4cf.b43ee96f7a8f34a004a676af50a9f778ceb1f1e8")
 DEFAULT_COMMANDS = (
     ("start", "Запустить бота"),
-    ("help", "Вывести справку")
+    ("help", "Вывести справку"),
+    ("hello world", "Привет, я-бот. Чем могу помочь?")
 )
