4ca7cd37ea0c4c8dad39492d93b6009ba8303260
рмухаммед асымбеков <ermuhammed_erema@mail.ru>
2024-04-15 11:43:39 +0500

    внесение изменений

--- handlers/default_handlers/__init__.py
+++ handlers/default_handlers/__init__.py
@@ -1,5 +1,10 @@
 from . import start
 from . import help
 from . import echo
+from . import low
+from . import high
+from . import custom
+from . import history
 
-__all__ = ['start', 'help', 'echo']
+
+__all__ = ['start', 'help', 'echo', 'low', 'high', 'custom', 'history']
